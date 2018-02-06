6223378dff858f352c5a4b87232b5b3d9593a030
Fayya Nadhira Anyatasia <anyatasia@gmail.com>
2018-02-06 14:50:02 +0700

    initial commit

---
+++ src/main/java/com/fanasia/phase2learn/Phase2LearnApplication.java
@@ -0,0 +1,12 @@
+package com.fanasia.phase2learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase2LearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase2LearnApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fanasia/phase2learn/Phase2LearnApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fanasia.phase2learn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Phase2LearnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
